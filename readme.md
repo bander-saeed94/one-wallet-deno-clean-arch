93104bc78d9afbc12d3f5b859cfda3b64503c53d
Bander Alshammari <bannadr1@hotmail.com>
2020-08-12 02:25:34 +0300

    add test cases for login use case

--- readme.md
+++ readme.md
@@ -3,19 +3,22 @@
 One Wallet. App to manage and track wallet where multiple persons are contributing.
 
 ## Use Cases  
-*  Register User
-*  Verify User
-*  Login User
-*  User create a wallet become admin.
-*  Admin invite other users to be contributors.
-*  User Accept invitation.
-*  User Deposit to wallet.
-*  Admin Confirm a deposit.
-*  Admin reject a deposit.
-*  Contributors track view wallet balance and transactions.
-*  Contributor request a loan.
-*  Contributors accept loan request.
-*  Contributor confirm receiving the loan.
+- [x] Register User
+- [x] Send Otp to User
+- [x] Verify User by Otp
+- [x] Login User
+- [ ] Forget Password
+- [ ] Reset Password
+- [ ] User create a wallet become admin.
+- [ ] Admin invite other users to be contributors.
+- [ ] User Accept invitation.
+- [ ] User Deposit to wallet.
+- [ ] Admin Confirm a deposit.
+- [ ] Admin reject a deposit.
+- [ ] Contributors track view wallet balance and transactions.
+- [ ] Contributor request a loan.
+- [ ] Contributors accept loan request.
+- [ ] Contributor confirm receiving the loan.
 
 
 ## Getting Started
--- tests/core/usecase/login-user-with-phone-number.test.ts
+++ tests/core/usecase/login-user-with-phone-number.test.ts
@@ -30,3 +30,51 @@ Deno.test("Login User", async () => {
   assertEquals(createdUser.firstName, loggedInUser.firstName);
   assertEquals(createdUser.lastName, loggedInUser.lastName);
 });
+
+Deno.test("Login User with invalid credentials", async () => {
+  //given
+  let testConfig = new TestConfig();
+  let createUserByPhoneNumberUseCase = testConfig
+    .createUserByPhoneNumberUseCase();
+  let createdUser = await createUserByPhoneNumberUseCase.createUser(
+    "966501766627",
+    "Aa123456",
+    "Bander",
+    "Alshammari",
+  );
+  //when
+  const loginUserWithPhoneNumberUseCase = testConfig
+    .loginUserWithPhoneNumberUseCase();
+  try {
+    let loggedInUser = await loginUserWithPhoneNumberUseCase.login(
+      "966501766627",
+      "Aa12345",
+    );
+  } catch (e) {
+    assertEquals(e.message, "password does not match");
+  }
+});
+
+Deno.test("Login with non existing user", async () => {
+  //given
+  let testConfig = new TestConfig();
+  let createUserByPhoneNumberUseCase = testConfig
+    .createUserByPhoneNumberUseCase();
+  let createdUser = await createUserByPhoneNumberUseCase.createUser(
+    "966501766627",
+    "Aa123456",
+    "Bander",
+    "Alshammari",
+  );
+  //when
+  const loginUserWithPhoneNumberUseCase = testConfig
+    .loginUserWithPhoneNumberUseCase();
+  try {
+    let loggedInUser = await loginUserWithPhoneNumberUseCase.login(
+      "966501766638",
+      "Aa123456",
+    );
+  } catch (e) {
+    assertEquals(e.message, "user with provided phoneNumber: 966501766638 not found");
+  }
+});
