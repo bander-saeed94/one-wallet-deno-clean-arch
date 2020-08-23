7e3c360fbc7ba6f6b2cebb3c494648e7d84f250a
Bander Alshammari <bannadr1@hotmail.com>
2020-08-23 08:02:05 +0300

    fix test

--- src/Adapters/gateways/sms-sender/SmsSenderImpl.ts
+++ src/Adapters/gateways/sms-sender/SmsSenderImpl.ts
@@ -1,7 +1,16 @@
 import SmsSender from "../../../UseCases/port/SmsSender.ts";
 
 export default class SmsSenderImpl implements SmsSender {
-  sendOtpForVerification(otp: string, to: string): void {
+  async sendOtpForVerification(otp: string, to: string): Promise<void> {
     console.log(`[sendOtpForVerification] send: "${otp}", to: ${to}`);
+  }
+  async sendResetPasswordLink(
+    link: string,
+    expireIn: Date,
+    to: string,
+  ): Promise<void> {
+    console.log(
+      `[sendResetPasswordLink] send: ${link} expire in: ${expireIn.toDateString()}, to: ${to}`,
+    );
   }
 }
